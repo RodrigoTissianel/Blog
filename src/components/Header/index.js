626cf98d977d8aba75ea34c9055a2c598cdf92e5
RodrigoTissianel <you@example.com>
2022-02-15 22:00:08 -0300

    Creating the structure and styling the header of the React Blog Project

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,24 @@
+import Logosvg from '../../svg/logo.svg';
+
 
 const Header = () =>{
+    const descLogo = "Essa á a descrição da logo do Blog.";
     return (
-        <h1>Header.js</h1>
+        <>
+        <header className="flex-space-between">
+            <div className="logo">
+                <img src={Logosvg} alt={descLogo}></img>
+            </div>
+            <div className="search">
+                <input type="text" name="search" className="input-search" placeholder="Buscar artigos, podcasts, destaques..."></input>
+            </div>
+            <ul className="menu">
+                <li> <a href="#" className="nav-link">Categories</a> </li>
+                <li> <a href="#" className="nav-link">About</a> </li>
+                <li> <a href="#" className="nav-link">Contact</a> </li>
+            </ul>
+        </header>
+        </>
     );
 };
 
