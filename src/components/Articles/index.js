82e8cf6c0251212809578521e05e7899e345ef25
RodrigoTissianel <you@example.com>
2022-02-16 21:52:03 -0300

    Added the props subtitle and title compponents to the Posts

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -4,16 +4,33 @@ import Posts from '../Posts';
 const Articles = () =>{
 
     return (
-        <>
+        
         <section className="container">
         <h1>Article.js</h1>
         <div className="container-posts mt-5">
-        <Posts/>
-        <Posts/>
-        <Posts/>
+        <Posts subtitle="tecnologia" title="O guia definitivo sobre o blog">
+            1lorem ipsum sit dolor amet ipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor amet
+        </Posts>
+
+        <Posts subtitle="Fotografia" title="Quais as melhores câmeras em 2021?">
+            2lorem ipsum sit dolor amet ipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor amet
+        </Posts>
+
+        <Posts subtitle="Cinema" title="Os 10 filmes com maior bilheteria">
+            3lorem ipsum sit dolor amet ipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
+            ipsum sit dolor ametipsum sit dolor amet
+        </Posts>
         </div>
         </section>
-        </>
+        
     );
 };
 
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,19 +1,16 @@
 
-const Posts = () =>{
+const Posts = (props) =>{
     return(
         <div className="Post mx-2">
             <div className="img-post"></div>
-            <h5>Tecnologia</h5>
-            <h3>O guia definitivo do blog</h3>
-            <p>lorem ipsum sit dolor amet ipsum sit dolor amet
-            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
-            ipsum sit dolor ametipsum sit dolor ametipsum sit dolor amet
-            ipsum sit dolor ametipsum sit dolor amet</p>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p>{props.children}</p>
             <div className="flex pt-2">
                 <div className="img-profile"></div>
                 <div className="desc-profile ml-2">
                     <h6 className="color-blue">Rodrigo Tissianel</h6>
-                    <p>Aug 2, 2020 - 8 min read</p>
+                    <p className="mt-1">Aug 2, 2020 - 8 min read</p>
                 </div>
             </div>
         </div>
